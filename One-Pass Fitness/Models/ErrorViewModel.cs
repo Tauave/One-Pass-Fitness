73d2bbe736fdbb6a6ec19e502435b46dd0825a3d
Tauave Tofa <ac155895@avcol.school.nz>
2026-03-17 14:06:02 +1300

    Add project files.

---
+++ One-Pass Fitness/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace One_Pass_Fitness.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
