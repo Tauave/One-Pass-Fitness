c7438cf93149b7c13a2f360be579bd15f5883fb4
Tauave Tofa <ac155895@avcol.school.nz>
2026-03-19 11:08:45 +1300

    Added Personalinformation Model

---
+++ One-Pass Fitness/Models/Personalinformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace One_Pass_Fitness.Models
+{
+    public class Personalinformation
+    {
+        public int Personid { get; set; }
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Lastname { get; set; }
+        [Required]
+        [StringLength (100)]
+
+        public DateOnly DOB { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Phone { get; set; }
+
+
+    }
+}
