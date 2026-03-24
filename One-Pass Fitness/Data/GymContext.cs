3f10c32eb9665d2d07f70a89f8493d6ddea69c69
ac155895 <ac155895@A27-GM0YSMEH>
2026-03-24 14:14:36 +1300

    Made the Database Context

---
+++ One-Pass Fitness/Data/GymContext.cs
@@ -0,0 +1,21 @@
+﻿using One_Pass_Fitness.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace One_Pass_Fitness.Data
+{
+    public class GymContext : DbContext
+    {
+        public GymContext(DbContextOptions<GymContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<BookingClasses> BookingClasses { get; set; }
+        public DbSet<Classes> Classes { get; set; }
+        public DbSet <Member> Members { get; set; }
+        public DbSet <MembershipInfo> Memberships { get; set; }
+        public DbSet <Trainers> Trainers { get; set; }
+        public DbSet <Personalinfo> Personalinfo { get; set; }
+
+    }
+}
