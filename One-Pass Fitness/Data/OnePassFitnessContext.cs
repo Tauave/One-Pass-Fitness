3367c04f67f5e86867647f8a29443af3844d79d3
Tauave Tofa <ac155895@avcol.school.nz>
2026-05-20 12:05:18 +1200

    Reverted

--- One-Pass Fitness/Data/OnePassFitnessContext.cs
+++ One-Pass Fitness/Data/OnePassFitnessContext.cs
@@ -1,16 +1,16 @@
 ﻿using One_Pass_Fitness.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace One_Pass_Fitness.Data
 {
-    public class OnePassFitnessContext : IdentityDbContext<Users>
+    public class OnePassFitnessContext : DbContext
     {
         public OnePassFitnessContext(DbContextOptions<OnePassFitnessContext> options) : base(options)
         {
         }
 
-       
+        public DbSet <Users> Users { get; set; }
+        public DbSet<Roles> Roles { get; set; }
         public DbSet<Classes> Classes { get; set; }
         public DbSet<Membership> Memberships { get; set; }
         public DbSet<Personalinfo> Personalinfo { get; set; }
@@ -18,8 +18,7 @@ namespace One_Pass_Fitness.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Personalinfo>().ToTable("Personalinfo");
-
-
+            modelBuilder.Entity<Roles>().ToTable("Roles");
 
             modelBuilder.Entity<Users>(e =>
             {
@@ -28,28 +27,31 @@ namespace One_Pass_Fitness.Data
                     .WithMany()
                     .HasForeignKey(u => u.Personid)
                     .OnDelete(DeleteBehavior.Restrict);
+                e.HasOne(u => u.Role)
+                    .WithMany()
+                    .HasForeignKey(u => u.RoleId)
+                    .OnDelete(DeleteBehavior.Restrict);
             });
 
             modelBuilder.Entity<Classes>(e =>
             {
                 e.ToTable("Classes");
-                e.HasOne(c => c.User)
-                    .WithMany(u => u.Classes)
-                    .HasForeignKey(c => c.UserId)
+                e.HasOne(c => c.Role)
+                    .WithMany()
+                    .HasForeignKey(c => c.RoleId)
                     .OnDelete(DeleteBehavior.Restrict);
             });
 
             modelBuilder.Entity<Membership>(e =>
             {
                 e.ToTable("Membership");
+                e.Property(m => m.MembershipType).HasMaxLength(100);
                 e.Property(m => m.Price).HasPrecision(18, 2);
-                e.HasOne(m => m.User)
-                    .WithMany(u => u.Memberships)
-                    .HasForeignKey(m => m.Userid)
+                e.HasOne(m => m.Roles)
+                    .WithMany()
+                    .HasForeignKey(m => m.Roleid)
                     .OnDelete(DeleteBehavior.Restrict);
             });
-
-
         }
     }
 }
